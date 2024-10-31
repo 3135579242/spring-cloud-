fc9e9307aff86524b47d19816469744a96aeddb1
Lee <3135579242@qq.com>
2024-10-31 21:04:43 +0800

    添加SpringCloud模版工程

---
+++ 002-SpringCloud模版工程/spring-cloud-alibaba-demo/service-demo/src/main/java/al/ocean/mall/controller/HelloController.java
@@ -0,0 +1,17 @@
+package al.ocean.mall.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
